a3fe00fd5cf707108c7db5057ce4a79c0a0f5b41
KeithLin724 <38067890+KeithLin724@users.noreply.github.com>
2021-09-24 14:49:19 +0800

    Update KYengine(easy)_main.cpp

--- main_file/KYengine(easy)_main.cpp
+++ main_file/KYengine(easy)_main.cpp
@@ -14,19 +14,16 @@ int main() {
     auto text = thing_tool.Build_text("hello", 2, 1, _KYF_color_::BLUE);
     auto backgrand = thing_tool.Build_background(_KYF_QBS_());
     auto graph = thing_tool.Build_graph("Spider_ASCII_art.txt");
+	
     var event = thing_tool.Build_event();
-
     var text2 = thing_tool.Build_text("very good", 25., 25.); 
     var graph2 = _KYbuild_::Build_graph("cartoon.txt"); 
-    //auto point = _KYbuild_::Build_point(0, 50); 
 
     graph.set_point(0,50);
     graph.set_color(_KYF_color_::RED); 
 
     graph2.set_color(_KYF_color_::GREEN); 
     graph2.set_point(15, 80); 
-
-    //text2 *= 3; 
 
     thing_tool.add(text); 
     thing_tool.add(text2); 
@@ -39,7 +36,6 @@ int main() {
     text2.eraser(); 
 
     while (event.get_event() != 'q') {
-		//std::cout << event.to_HEX() << "\n";
         if (event.get_event_memory() == 'w') {
             graph2.up();
         }
@@ -67,28 +63,3 @@ int main() {
 
     return 0; 
 }
-
-
-/*int main() {
-    KY_engine::Game_Text out_text("hello", 2., 1., KY_engine::Game_color::BLUE);
-
-     KY_engine::Quick_backgrand_setup quit_set_up;
-     KY_engine::Game_Backgrand game_backgrand(quit_set_up);
-
-     KY_engine::Game_graph spider_man("Spider_ASCII_art.txt");
-
-
-
-     spider_man.set_point(0, 50);
-
-     game_backgrand.draw();
-     out_text.write();
-
-     spider_man.graph_draw();
-
-     KY_engine::Graph_Window MOVE;
-     MOVE.gotoxy(END_POINT);
-
-    
-    return 0;
-}*/
